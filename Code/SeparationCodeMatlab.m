e92596d019541c932565251a2bc946ba721d9bd5
Shrey Bohra <42218014+shreybohra@users.noreply.github.com>
2023-04-04 23:46:03 +0100

    fixes

--- Code/SeparationCodeMatlab.m
+++ Code/SeparationCodeMatlab.m
@@ -1,5 +1,8 @@
 clear 
 close all
+clc
+cols = ["#FAC05E", "#59CD90", "#EE6352", "#3FA7D6"];
+
 % Variables 
 
 % Importing data into an array 
@@ -29,21 +32,29 @@ tic
 fr = 20;
 fs = 20;
 lw = 2;
-ms = 20;
+ms = 14;
 af = figure;
+grid minor
 hold on
 set(af, 'Position', [0 0 2560 1440]);
-plot(-10, 0, 'kx', 'LineWidth', lw, 'MarkerSize', ms)
-plot(-10, 0, 'gx', 'LineWidth', lw, 'MarkerSize', ms)
-plot(-10, 0, 'r-', 'LineWidth', lw, 'MarkerSize', ms)
+plot(-10, 0, 'x', 'LineWidth', lw, 'MarkerSize', ms, 'Color', cols(1))
+plot(-10, 0, 'x', 'LineWidth', lw, 'MarkerSize', ms, 'Color', cols(2))
+plot(-10, 0, '-', 'LineWidth', lw, 'MarkerSize', ms, 'Color', cols(3))
+plot(-10, 0, '-', 'LineWidth', lw, 'MarkerSize', ms, 'Color', cols(4))
+
 xlim([200 400])
 ylim([min(atmospalt(data(:, 2)))-10, 600])
-legend('Prelaunch', 'Launch Detected', 'Predicted Trajectory','AutoUpdate','off', 'FontSize', fs)
+ylabel('Altitude (m)')
+xlabel('Time (s)')
+legend('Prelaunch', 'Actual Trajectory', 'Current Predicted Trajectory', ...
+    'Predicted Trajectory at Separation Trigger','AutoUpdate','off', 'FontSize', fs)
 caption = text(200, 550, sprintf('Launch not detected, t = 0'));
 
-v = VideoWriter('apogee_fit.mp4','MPEG-4');
+v = VideoWriter('apogee_fit_astra2.mp4','MPEG-4');
 v.FrameRate = fr;
 v.Quality = 98;
+
+open(v);
 
 for i = 5000:6000 % Taking in data point by point - to mimic the pressure readings. 
     availabledata(i,1) = data(i,1); % Adding each time datapoint onto the end of all data array. 
@@ -90,27 +101,36 @@ for i = 5000:6000 % Taking in data point by point - to mimic the pressure readin
             
             if exist('trajpred')
                 delete(trajpred)
-                delete(predap)
+%                 delete(predap)
             end
-            trajpred = plot((anim_xs + launchData(1,1))/1000, anim_ys, 'r-', 'LineWidth', lw, 'MarkerSize', ms);
-            predap = text(340, 300, sprintf('Apogee prediction %.1f m at t = %.2f', ag2, tguess), 'FontSize', fs);
-            if ag2 - ag1 < 1 && launchData(end, 2) <= ag2
+            trajpred = plot((anim_xs + launchData(1,1))/1000, anim_ys, '-', 'LineWidth', lw, 'MarkerSize', ms, 'Color', cols(3));
+%             predap = text(300, 300, sprintf('Current Apogee prediction %.1f m at t = %.2f', ag2, tguess/1000), 'FontSize', fs);
+            if ag2 - ag1 < 1 && launchData(end, 2) <= ag2 && launchData(end, 1) > tguess
                 vel = velcoeffs(1)*Ts(end) + velcoeffs(2);
-                fprintf('Apogee detected at t = %.1f, alt = %.1f, v = %.2f\n', Ts(end)/1000, As(end), vel)
-                apogeeflag = text(340, 450, 'Apogee Detected!');
+                fprintf('Apogee detected at t = %.1f, alt = %.1f, v = %.2f\n', Ts(end)/1000, ag2, vel)                
+                if ~exist('apogeeflag')
+                    plot((anim_xs + launchData(1,1))/1000, anim_ys, '-', 'LineWidth', lw, 'MarkerSize', ms, 'Color', cols(4));
+                    xline(tguess/1000, 'k--', 'LineWidth', lw);
+                    yline(ag2, 'k--', 'LineWidth', lw);
+                    apogeeflag = text(240, 260, sprintf('Seperation Triggered at alt = %.1f t = %.2f', ag2, Ts(end)/1000), 'FontSize', fs);
+                end
             end
         end
-        plot(availabledata(1:min(val), 1)/1000, availabledata(1:min(val), 3), 'kx', 'LineWidth', lw, 'MarkerSize', ms)
-        plot(availabledata(min(val):end, 1)/1000, availabledata(min(val):end, 3), 'gx', 'LineWidth', lw, 'MarkerSize', ms)
-        caption = text(240, 550, sprintf('Launch detected, t = %.2f', availabledata(end, 1)/1000), 'FontSize', fs);
+        plot(availabledata(1:min(val), 1)/1000, availabledata(1:min(val), 3), 'x', 'LineWidth', lw, 'MarkerSize', ms, 'Color', cols(1))
+        plot(availabledata(min(val):end, 1)/1000, availabledata(min(val):end, 3), 'x', 'LineWidth', lw, 'MarkerSize', ms, 'Color', cols(2))
+        caption = text(220, 300, sprintf('Launch detected, t = %.2f', availabledata(end, 1)/1000), 'FontSize', fs);
 
     else
-        plot(availabledata(:, 1)/1000, availabledata(:, 3), 'kx', 'LineWidth', lw, 'MarkerSize', ms)
-        caption = text(240, 550, sprintf('Launch not detected, t = %.2f', availabledata(end, 1)/1000), 'FontSize', fs);
+        plot(availabledata(:, 1)/1000, availabledata(:, 3), 'x', 'LineWidth', lw, 'MarkerSize', ms, 'Color', cols(1))
+        caption = text(220, 300, sprintf('Launch not detected, t = %.2f', availabledata(end, 1)/1000), 'FontSize', fs);
     end
 
     drawnow
+    writeVideo(v, getframe(af));
 end
+
+close(v)
+
 
 function ys = coeffplot(xs, coeffs)
 
