98f389190d6a45d553ba09456af2f1bb667d8d37
Shrey Bohra <42218014+shreybohra@users.noreply.github.com>
2023-04-04 21:54:19 +0100

    adjustable trigger alt

--- Code/SeparationCodeMatlab.m
+++ Code/SeparationCodeMatlab.m
@@ -19,6 +19,7 @@ refpressure = 101728.25;
 maxthrustaltitude = 220;
 ag1 = 300; % Apogee guess 1.
 ag2 = 350; % Apogee guess 2. 
+min_triggeralt = 10;
 
 for i = 1: length(data) % Taking in data point by point - to mimic the pressure readings. 
     alldata(i,1) = data(i,1); % Adding each time datapoint onto the end of all data array. 
@@ -26,8 +27,8 @@ for i = 1: length(data) % Taking in data point by point - to mimic the pressure 
     alldata(i,3) = 44330 * ( 1 - (alldata(i,2)/refpressure)^(1/5.255)); % calculating the altitude at each pressure point. 
     
     % checking if the altitude is above 10m. 
-    if alldata(i,3) > 10
-        val = find(alldata(:,3)>10); % finding out at which points the altitude is more than 10m.
+    if alldata(i,3) > min_triggeralt
+        val = find(alldata(:,3)>min_triggeralt); % finding out at which points the altitude is more than 10m.
         for j = 1: length(val)
             % creating a new lauch data array - for all datapoints where
             % altitude > 10m.
