cd03057126ac98faf38b6e9f9825bc9d1a99f087
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2023-04-10 23:08:31 +0100

    bug fixes + build script

---
+++ apogee_detection_test/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ -I apogee_detection_test/eigen-3.4.0/eigen-3.4.0 apogee_detection_test/main.cpp lib/ApogeeDetect/apogee_detect.cpp
--- apogee_detection_test/main.cpp
+++ apogee_detection_test/main.cpp
@@ -4,9 +4,19 @@
 #include <vector>
 #include <exception>
 #include "../lib/ApogeeDetect/apogee_detect.h"
+
+const float refpressure = 101728.25;
+
+float pressure_to_altitude(const float pressure) {
+    return 44330 * (1 - std::pow(pressure/refpressure, 1/5.255));
+}
  
 int main(int argc, char **argv) {
-    std::ifstream data("./Code/astra2_air.txt");
+    if (argc < 2) {
+        throw std::invalid_argument("No data file specified");
+    }
+
+    std::ifstream data(argv[1]);
     
     std::vector<uint32_t> times;
     std::vector<float> pressures;
@@ -17,20 +27,25 @@ int main(int argc, char **argv) {
         std::stringstream ss(line);
 
         std::getline(ss, word, ',');
-        pressures.push_back(std::stof(word));
-        std::getline(ss, word, ',');
         times.push_back(std::stoul(word));
+        std::getline(ss, word, ',');
+        pressures.push_back(std::stof(word));
     }
 
     data.close();
 
     ApogeeDetect detector(times.at(0), 20);
 
-    for (int i = 0; i < times.size(); i++) {
-        uint32_t time = times[i];
-        float pressure = pressures[i];
+    uint32_t time;
+    float pressure;
 
-        const auto& info = detector.check_apogee(time, pressure);
+    for (int i = 0; i < times.size(); i++) {
+        if (pressure == pressures[i]) continue;
+
+        time = times[i];
+        pressure = pressures[i];
+
+        const auto& info = detector.check_apogee(time, pressure_to_altitude(pressure));
 
         std::cout << info.alt << "," << info.reached << "," << info.time << std::endl;
     }
